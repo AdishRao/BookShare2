21c7704b740da5d136c7a65844320d706341d097
AymanS23 <sher.ayman@gmail.com>
2017-09-12 23:16:15 +0530

    Continue this

--- app/src/main/java/lw/bookshare/MainActivity.java
+++ app/src/main/java/lw/bookshare/MainActivity.java
@@ -3,15 +3,44 @@ package lw.bookshare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import static android.R.attr.id;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+private Button Register_Button;
+private EditText email_New;
+private EditText password_New;
+private TextView existing_user_signin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+      Register_Button = (Button) findViewById(R.id.Register_Button);
+      email_New = (EditText) findViewById(R.id.email_New);
+      password_New = (EditText) findViewById(R.id.password_New);
+      existing_user_signin = (TextView) findViewById(R.id.existing_user_signin);
+
+      Register_Button.setOnClickListener(this);
+      existing_user_signin.setOnClickListener(this);
+
+    }
+    private void registerUser(){
+        String email =
+    }
+
+
+
+
+    public void onClick(View view){
+       if(view==Register_Button) {
+           registerUser();
+       }
+       if(view==existing_user_signin) {
+
+       }
     }
 }
