79e15cf09c3fb9d6cdc2667fdbccc8300fa9b8ca
Adish <adish.rao98@gmail.com>
2017-09-13 00:46:52 +0530

    Register done.

--- app/src/main/java/lw/bookshare/MainActivity.java
+++ app/src/main/java/lw/bookshare/MainActivity.java
@@ -1,24 +1,60 @@
 package lw.bookshare;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import static android.R.attr.id;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MainActivity";
 private Button Register_Button;
 private EditText email_New;
 private EditText password_New;
 private TextView existing_user_signin;
+ private ProgressDialog progressDialog;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+
+        };
+
+
+        progressDialog = new ProgressDialog(this);
       Register_Button = (Button) findViewById(R.id.Register_Button);
       email_New = (EditText) findViewById(R.id.email_New);
       password_New = (EditText) findViewById(R.id.password_New);
@@ -28,19 +64,66 @@ private TextView existing_user_signin;
       existing_user_signin.setOnClickListener(this);
 
     }
-    private void registerUser(){
-        String email =
+    private void registerUser() {
+        String email = email_New.getText().toString().trim();
+        String password = password_New.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this,"Enter Email",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this,"Enter Password",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        progressDialog.setMessage("Registering");
+        progressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                        Toast.makeText(MainActivity.this,"SUCCESSFUL",Toast.LENGTH_SHORT).show();
+                        }
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this,"Failled",Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+
     }
 
 
 
-
+    @Override
     public void onClick(View view){
        if(view==Register_Button) {
            registerUser();
        }
        if(view==existing_user_signin) {
+           //to be filled
 
        }
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
 }
