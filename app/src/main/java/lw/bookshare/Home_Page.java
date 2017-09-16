000686e9c8abd2ea3751d4e1b4a3394d32e5ecc7
a5hw1n1 <ashwinikelkar23@gmail.com>
2017-09-16 16:45:46 +0530

    home

--- app/src/main/java/lw/bookshare/Home_Page.java
+++ app/src/main/java/lw/bookshare/Home_Page.java
@@ -12,6 +12,8 @@ import com.google.firebase.auth.FirebaseUser;
 public class Home_Page extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth mAuth;
+ Button btn_search;
+    Button btn_edit;
 
     private Button buttonLogout;
     @Override
@@ -21,9 +23,10 @@ public class Home_Page extends AppCompatActivity implements View.OnClickListener
 
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser user = mAuth.getCurrentUser();
-
-
-
+        btn_search =(Button) findViewById(R.id.btn_search);
+        btn_edit=(Button) findViewById(R.id.btn_edit);
+        btn_search.setOnClickListener(this);
+        btn_edit.setOnClickListener(this);
 
         buttonLogout =(Button) findViewById(R.id.buttonLogout);
         buttonLogout.setOnClickListener(this);
@@ -37,6 +40,17 @@ public class Home_Page extends AppCompatActivity implements View.OnClickListener
             mAuth.signOut();
             finish();
             startActivity(new Intent(this,Login_Page.class));
+        }
+        if(view==btn_search)
+        {
+            finish();
+            startActivity(new Intent(this,edit_Library.class));
+        }
+
+        if(view==btn_edit)
+        {
+            finish();
+            startActivity(new Intent(this,edit_Library.class));
         }
 
     }
